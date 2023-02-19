bfb86fcaade7c331db5d796c135122b0de6fdee4
Delphiniki <delphi5@abv.bg>
2023-02-19 11:28:00 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -14,7 +14,7 @@ lastdir1=$(find ./ -type d -name 'sudo-*')
 cd $lastdir1
 echo $(pwd)
 sleep 2
-sudo -uroot ./configure --prefix=/usr --libexecdir=/usr/lib --with-secure-path --with-all-insults --with-env-editor --docdir=/usr/share/doc/$lastdir --with-passprompt="[>
+sudo -uroot ./configure --prefix=/usr --libexecdir=/usr/lib --with-secure-path --with-all-insults --with-env-editor --docdir=/usr/share/doc/$lastdir --with-passprompt="[sudo] password for %p: "
 sudo -uroot make
 sudo -uroot make install && sudo -uroot ln -sfv libsudo_util.so.0.0.0 /usr/lib/sudo/libsudo_util.so.0
 
