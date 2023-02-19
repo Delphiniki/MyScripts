49a31fde96cac8fbcea4d582a011dcc0618fec8a
Delphiniki <delphi5@abv.bg>
2023-02-19 11:36:28 +0200

    Update sudo_updater.sh

--- sudo_updater.sh
+++ sudo_updater.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-sudo apt-get update
+sudo apt-get update -y && \
 tempd=$(mktemp -d)
 cd $tempd
-echo $(pwd)
-sleep 2
 wget https://www.sudo.ws/dist/sudo.tar.gz
 tar -zxvf ./sudo.tar.gz
-echo "Done with extraction of files!"
-sleep 2
-lastdir1=$(find ./ -type d -name 'sudo-*')
+#echo "Done with extraction of files!"
+sudodir=$(find ./ -type d -name 'sudo-*')
 #echo $lastdir1
-cd $lastdir1
-echo $(pwd)
-sleep 2
-sudo -uroot ./configure --prefix=/usr --libexecdir=/usr/lib --with-secure-path --with-all-insults --with-env-editor --docdir=/usr/share/doc/$lastdir1 --with-passprompt="[sudo] password for %p: "
+cd $sudodir
+#echo $(pwd)
+sudo -uroot ./configure --prefix=/usr --libexecdir=/usr/lib --with-secure-path --with-all-insults --with-env-editor --docdir=/usr/share/doc/$sudodir --with-passprompt="[sudo] password for %p: "
 sudo -uroot make
 sudo -uroot make install && sudo -uroot ln -sfv libsudo_util.so.0.0.0 /usr/lib/sudo/libsudo_util.so.0
 
