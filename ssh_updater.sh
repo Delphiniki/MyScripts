62d9c08d65db36aa0c07e9d00a3b993cd9e31fdd
Delphiniki <delphi5@abv.bg>
2023-02-20 08:40:03 +0200

    Added openssh package update script

---
+++ ssh_updater.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script upgrades openssh package to version 9.2p1
+
+sudo apt update && \
+sudo apt install build-essential zlib1g-dev libssl-dev -y && \
+sudo mkdir /var/lib/sshd
+sudo chmod -R 700 /var/lib/sshd/
+sudo chown -R root:sys /var/lib/sshd/
+# change the openssh version in next lines #
+cd /tmp
+wget -c https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.2p1.tar.gz
+sshfile=$(sudo find ./ -type f -name "openssh-*.tar.gz")
+tar -xzf $sshfile
+sshdir=$(sudo find ./ -type d -name "openssh-*")
+cd $sshdir
+sudo apt install libpam0g-dev libselinux1-dev libkrb5-dev -y && \
+sudo ./configure --with-kerberos5 --with-md5-passwords --with-pam --with-selinux --with-privsep-path=/var/lib/sshd/ --sysconfdir=/etc/ssh
+sudo make
+sudo -uroot make install
+exit 0
